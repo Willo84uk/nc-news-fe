e8c87fdabee469f427266f0a8a16db4efad301ae
Ronnie Williams <ronniewilliams06@aol.com>
2023-12-11 13:54:17 +0000

    update branch

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,17 +1,15 @@
 function ArticleCard({ article }) {
-
-    
-    if(article){
-      const createdAt = new Date (article.created_at)
-  return (
-    <div className="articlecard"> 
-    <img className="articlelistimg" src={article.article_img_url}></img>
-      <h3> {article.title} Article Card</h3>
-      <div>Votes {article.votes}</div>
-      <div>Article created {createdAt.toGMTString()}</div>
-    </div>
-  );
-}
+  if (article) {
+    const createdAt = new Date(article.created_at);
+    return (
+      <div className="articlecard">
+        <div>Votes {article.votes}</div>
+        <img className="articlelistimg" src={article.article_img_url}></img>
+        <h3> {article.title} Article Card</h3>
+        <div>Article created {createdAt.toGMTString()}</div>
+      </div>
+    );
+  }
 }
 
 export default ArticleCard;
