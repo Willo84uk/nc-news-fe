a76bed3c8750db38569aad478bc478bd4d10532a
Ronnie Williams <ronniewilliams06@aol.com>
2023-12-11 14:02:21 +0000

    update vote count and date created to p tags inplace of divs

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,10 +3,10 @@ function ArticleCard({ article }) {
     const createdAt = new Date(article.created_at);
     return (
       <div className="articlecard">
-        <div>Votes {article.votes}</div>
+        <p>Votes {article.votes}</p>
         <img className="articlelistimg" src={article.article_img_url}></img>
         <h3> {article.title}</h3>
-        <div>Article created {createdAt.toGMTString()}</div>
+        <p>Article created {createdAt.toGMTString()}</p>
       </div>
     );
   }
